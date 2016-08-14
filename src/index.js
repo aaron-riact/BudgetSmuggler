7467d70f9f98325e66f8fd67b4a8f0d8060a4bf3
Aaron Lipinski <kris.lipinski@gmail.com>
2016-08-14 17:29:54 +1200

    move appState to appState.js

--- src/index.js
+++ src/index.js
@@ -1,72 +1,7 @@
 const yo = require('yo-yo');
-const {observable, autorun, computed} = require("mobx");
+const {appState, makeNewChild} = require('./appState')
+const {autorun} = require("mobx");
 
-const appState = observable({
-    editing: false,
-    total: function() { console.log('TOT'); return sumChildren(this.sections) },
-  	sections: [
-      {
-      	name: 'section 1',
-        open: true,
-        values: function() { console.log('ZZ'); return  sumChildren(this.categories) }, 
-      	categories: [
-          {
-            name: 'category 1',
-            open: true,
-            values: function() { console.log('YY'); return sumChildren(this.extendeds) },
-            extendeds: [
-              {
-                name: 'extended 1',
-                values: [1,2,3,4,5,6,7,8,9,10,11,12]
-              },
-              {
-                name: 'extended 2',
-                values: [1,2,3,4,5,6,7,8,9,10,11,12]
-              },
-              {
-                name: 'extended 3',
-                values: [1,2,3,4,5,6,7,8,9,10,11,12]
-              },
-            ]
-          }
-        ]
-  		},
-      {
-        name: 'section 2',
-        open: true,
-        values: function() { console.log('SEC'); return sumChildren(this.categories) },
-        categories: [
-        	{
-            name: 'category 1',
-            open: true,
-            values: function() { console.log('CAT'); return sumChildren(this.extendeds) },
-            extendeds: [
-              {
-                name: 'extended 1',
-                values: [1,2,3,4,5,6,7,8,9,10,11,12] 
-              },
-              {
-                name: 'extended 2',
-                values: [1,2,3,4,5,6,7,8,9,10,11,12]
-              },
-              {
-                name: 'extended 3',
-                values: [1,2,3,4,5,6,7,8,9,10,11,12]
-              },
-            ]
-          }
-        ]
-      },
-    ]
-});
-
-const sumChildren = (children) =>
-  children
-    .map(node => node.values)
-    .reduce((vals1, vals2) =>
-      vals1.map((v,i) => (v || 0) + (vals2[i] || 0) ), [0,0,0,0,0,0,0,0,0,0,0,0]
-    )
-        
 const onClick = (node, ev) => { 
   //console.log(computed);
   ev.preventDefault()
@@ -94,23 +29,6 @@ const makeRow = (node, klass) =>
     )}
   </ol>`
         
-const makeNewChild = (parent, parentClass, e) => {
-  if(e) e.stopPropagation(); 
-  if(parentClass==='category') parent.extendeds.push({name: 'ext', values:Array(12)}); 
-  if(parentClass==='section') parent.categories.push({
-    name: 'cat',
-    open: true,
-    values:function() { console.log('CATN', this); return sumChildren(this.extendeds) },
-    extendeds: []
-  });
-  if(parentClass==='root') parent.sections.push({
-    name: 'sec',
-    open: true,
-    values: function() { console.log('ZZ'); return  sumChildren(this.categories) },
-    categories: []
-  })
-}
-
 const toggleEditMode = () =>
   appState.editing = !appState.editing
 
