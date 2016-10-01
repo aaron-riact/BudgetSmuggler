717dcc9e1fa3d57c891c266d414db6adf996fac5
Aaron Lipinski <kris.lipinski@gmail.com>
2016-10-01 14:53:39 +1300

    try redom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,79 @@
-const yo = require('yo-yo');
+const mobx = require('mobx')
 const {appState, makeNewChild, init, currentOffset} = require('./appState')
-const {autorun} = require("mobx");
+const { el, list, mount } = require('redom')
+
+const hello = el('h1', 'Hello world!');
+
+const nextBudget = (amt) =>
+  appState.currentBudget = Math.abs((appState.currentBudget + amt) % appState.budgets.length)
+
+mount(document.body, hello);
+const renderTitle = () =>
+  el('div.title',
+    el('div.left',
+      el('button.edit-button', 'Edit')
+    ),
+    el('div.center',
+      el('h3', 'Budget Smuggler')),
+    el('div.right.year-chooser',
+      el('button', {onclick: () => nextBudget(-1)}, 'B'),
+      el('button', {onclick: () => nextBudget(+1)}, 'F')
+    )
+  )
+
+const renderEditButton = (root, rootClass) => {
+  const name = rootClass === 'root' ? 'sec' : rootClass === 'section' ? 'cat' : 'ext'
+  return appState.editing ?
+    el('div.new',
+      el('button', {class: rootClass, onclick: makeNewChild.bind(null, root, rootClass, name)},
+        '+'
+      )
+    ) : ''
+}
+
+const getBudgetSize = () => appState.budgets[appState.currentBudget]
+const getColArray = () =>
+  Array.from({length: getBudgetSize()}, (_, i) => i + currentOffset())
+
+const makeRow = (node, klass) =>
+  el('ol',
+    ...getColArray().map(month =>
+      el('li',
+        klass === 'extended'
+          ? el('input', {
+            value: node.values[month] || 0,
+            oninput: ev => node.values[month] = parseFloat(ev.target.value) || 0 
+          })
+          : node.values[month] || '0' 
+      )
+    )
+  )
+
+const getMonthsArray = () => {
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const budgetSize = getBudgetSize()
+  const array = Array.from({length: getBudgetSize() + currentOffset()},
+    (_, i) => months[(i + appState.startMonth) % 12]
+  )
+  console.log('array', array)
+  return array
+}
+
+const renderMonths = () =>
+  el('div.header',
+    el('div.nav',
+      el('span.name', 'Months')
+    ),
+    renderEditButton(appState, 'root'),
+    el('div.months',
+      makeRow({values: getMonthsArray()})
+    )
+  )
+
+function onBlur (node, ev) {
+  node.name = ev.target.value
+  appState.editingNode = null
+}
 
 const onClick = (node, ev) => { 
   //console.log(computed);
@@ -13,140 +86,110 @@ const onClick = (node, ev) => {
 	node.open = !node.open
 }
 
-const onFocus = (node, ev) => {
-  console.log('onFocus', ev)
-}
-
-var range = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i);
-const getBudgetSize = () => appState.budgets[appState.currentBudget]
-const getColArray = () =>
-  Array.from({length: getBudgetSize()}, (_, i) => i + currentOffset())
-
-const getMonthsArray = () => {
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  const budgetSize = getBudgetSize()
-  const array = Array.from({length: getBudgetSize() + currentOffset()},
-    (_, i) => months[(i + appState.startMonth) % 12]
+const makeNode = (node, klass, children = []) =>
+  el('div', {class: klass, onclick: onClick.bind(null, node), onfocus},
+    el('div.nav',
+      appState.editingNode === node
+      ? el('input', {value: node.name, onblur: onBlur.bind(null, node)})
+      : el('span.name', node.name)
+    ),
+    renderEditButton(node, klass),
+    el('div.months', makeRow(node, klass)),
+    el('div.children', ...children)
   )
-  console.log('array', array)
-  return array
-}
 
-const makeRow = (node, klass) =>
-  yo`<ol>
-    ${getColArray().map(month =>
-      yo`<li>
-        ${klass === 'extended'
-          ? yo`<input value="${node.values[month] || 0}" oninput=${ev => node.values[month] = parseFloat(ev.target.value) || 0}>`
-      		: node.values[month] || 0
-				}
-      </li>`
-    )}
-  </ol>`
-        
-const toggleEditMode = () =>
-  appState.editing = !appState.editing
-
-const nextBudget = (amt) =>
-  appState.currentBudget = Math.abs((appState.currentBudget + amt) % appState.budgets.length)
-
-const renderTitle = () =>
-  yo`
-    <div class='title'>
-      <div class='left'>
-        <button class='edit-button' onclick=${toggleEditMode}>
-          Edit
-        </button>
-      </div>
-      <div class='center'>
-        <h3>
-          Budget Smuggler
-        </h3>
-      </div>
-      <div class='right year-chooser'>
-        <button onclick=${() => nextBudget(-1)}>B</button>
-        <button onclick=${() => nextBudget(+1)}>F</button>
-      </div>
-    </div>
-  `
-  
-const renderEditButton = (root, rootClass) => {
-  const name = rootClass === 'root' ? 'sec' : rootClass === 'section' ? 'cat' : 'ext'
-  return appState.editing ? yo`
-    <div class='new'>
-      <button class=${rootClass} onclick=${makeNewChild.bind(null, root, rootClass, name)}>
-        +
-      </button>
-    </div>
-  ` : ''
-}
-
-const renderMonths = () =>
-  yo`<div class='header'>
-    <div class='nav'>
-      <span class='name'>Months</span>
-    </div>
-    ${renderEditButton(appState, 'root')}
-    <div class='months'>
-      ${makeRow({values: getMonthsArray()})}
-    </div>
-  </div>
-  ` 
-
-function onBlur (node, ev) {
-  node.name = ev.target.value
-  appState.editingNode = null
-}
-
-const makeNode = (node, klass, children) =>
-   yo`<div class=${klass} onclick=${onClick.bind(null, node)} onfocus=${onFocus}>
-    <div class='nav'>
-      ${appState.editingNode == node
-        ? yo`<input value=${node.name} onblur=${onBlur.bind(null, node)}/>`
-    	  : yo`<span class='name'>${node.name}</span>`
-      }
-    </div>
-    ${renderEditButton(node, klass)}
-    <div class='months'>
-      ${makeRow(node, klass)}
-    </div>
-    <div class='children'>
-      ${children || undefined}
-    </div>
-  </div>`
-          
 const isOpen = (node) =>
   node.open || appState.editing
 
 const renderTree = sections =>
-  yo`<div class='root'>
-  	${sections.map(section =>
+  el('div.root',
+    ...sections.map(section =>
       makeNode(section, 'section',
-  			isOpen(section) && section.categories.map(category =>
-      		makeNode(category, 'category',
-      			isOpen(category) && category.extendeds && category.extendeds.map(extended =>
-     				 	makeNode(extended, 'extended')
+        isOpen(section) && section.categories.map(category =>
+          makeNode(category, 'category',
+            isOpen(category) && category.extendeds && category.extendeds.map(extended =>
+              makeNode(extended, 'extended')
             )
-      		)
-  			)
+          )
+        )
       )
-  	)}
-  </div>` 
+    )
+  )
 
-const inputComp = state =>
-  yo`<input value="${state.name}" oninput=${ev => state.name = ev.target.value}>`
-
-const appComp = state => yo`<div class='container'>
-	<div class='budget'>
-    ${renderTitle()}
-    ${renderMonths()}
-		${renderTree(state.sections)}
-  	${makeNode({name: 'Total', values: state.total}, 'section total')}
-  </div>
-</div>`;
-
-const ref = document.body.appendChild(document.createElement('div')); 
+class TotalRow {
+  constructor() {
+    this.el = el('div')
+  }
+  update(state) {
+    this.el = makeNode({name: 'Total', values: state.total}, 'section total')
+  }
+}
+const totalRow = new TotalRow
+const appComp = state =>
+  el('div.container',
+    el('div.budget',
+      renderTitle(),
+      renderMonths(),
+      renderTree(state.sections),
+      totalRow.update(state),
+      makeNode({name: 'Total', values: state.total}, 'section total')
+    )
+  )
 
 init()
-const x = autorun(() => yo.update(ref, appComp(appState)));
-//console.log('donex', x); 
+console.log('appState', appState)
+const ref = document.body.appendChild(document.createElement('div')); 
+mount(ref, appComp(appState))
+// autorun(() => console.log('upda'))
+//const x = autorun(() => yo.update(ref, appComp(appState)));
+/******************* TD */
+class Td {
+  constructor () {
+    this.el = el('td');
+    console.log('td cons')
+  }
+  update (data) {
+    mobx.autorun(() => {
+      this.el.textContent = data.a;
+      //console.log('td upda', data)
+      //console.dir(data)
+    })
+  }
+}
+
+const Tr = list.extend('tr', Td);
+const Table = list.extend('table', Tr);
+
+const table = new Table;
+
+mount(document.body, table);
+
+table.update([
+  [ {a: 1}, {a: 2}, {a: 3} ],
+  [ {a: 4}, {a: 5}, {a: 6} ],
+  [ {a: 7}, {a: 8}, {a: 9} ]
+]);
+
+const z = mobx.observable([
+  [ {a: 1}, {a: 2}, {a: 3} ],
+  [ {a: 4}, {a: 5}, {a: 6} ],
+  [ {a: 7}, {a: 8}, {a: 9} ]
+])
+
+table.update(z)
+console.log('z', z)
+/**************** Li */
+class Li {
+  constructor () {
+    this.el = el('li')
+  }
+  update (data) {
+    this.el.textContent = data
+  }
+}
+var ul = list('ul', Li)
+mount(document.body, ul)
+ul.update([ 1, 2, 3 ].map(i => 'Item ' + i))
+// mobx.autorun(() => table.update(z))
+
+window.z = z
